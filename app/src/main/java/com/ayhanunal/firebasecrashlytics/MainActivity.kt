da86aa308f348bbb31862869502dbe5857fea69f
ayhanunal <unalayhan@yahoo.com>
2021-05-11 21:48:27 +0300

    crash added

--- app/src/main/java/com/ayhanunal/firebasecrashlytics/MainActivity.kt
+++ app/src/main/java/com/ayhanunal/firebasecrashlytics/MainActivity.kt
@@ -2,10 +2,22 @@ package com.ayhanunal.firebasecrashlytics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        create_button.setOnClickListener {
+            throw RuntimeException("Crash") // Force a crash
+        }
+
+        division_zero_error.setOnClickListener {
+            var result = 1/0
+        }
+
+
     }
 }
